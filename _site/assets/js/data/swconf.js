26649e87bc60b1ecc47ae7e9e7cbf90981d2541c
Rúben Ribeiro Garcia <rubenribeirogarcia@gmail.com>
2025-01-21 11:41:43 +0000

    chore: update favicon

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737307459',resources: [
+    cacheName: 'chirpy-1737459637',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
